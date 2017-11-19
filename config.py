972be2acfaac197b8ee1c1dc3616765adcaedf0b
Ilya Zverev <zverik@textual.ru>
2017-11-19 16:58:45 +0300

    wsgi

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 
 DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'streak.db')
 # DATABASE_URI = 'postgresql://localhost/osmstreak'
